dd1a7e355640b6d2e0c72ba4099c3fd865397f68
Vineta <Vineta@LAPTOP-9KVL4IOV>
2021-10-07 20:44:20 +0300

    Add project files.

---
+++ RigaSchool.iu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RigaSchool.iu
+{
+    class RigaSchool1iu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("My name is Vineta."); Console.Write("I am 37 years old.");
+            Console.WriteLine("I enjoy reading and pondering God's Word on a daily basis."); Console.Write("I like music.");
+            Console.WriteLine("Worship Songs are special to me.");
+            Console.WriteLine("I like to learn something new and challenging.");
+            Console.ReadLine();
+        }
+    }
+}
