a2a51817e17af385cb9d35850bb4a1be3fb080af
vinneija <neija.vineta@gmail.com>
2021-10-17 17:04:47 +0300

    CCC

--- RigaSchool.iu/Program.cs
+++ RigaSchool.iu/Program.cs
@@ -11,6 +11,8 @@ namespace RigaSchool.iu
             Console.WriteLine("I enjoy reading and pondering God's Word on a daily basis."); Console.Write("I like music.");
             Console.WriteLine("Worship Songs are special to me.");
             Console.WriteLine("I like to learn something new and challenging.");
+            Console.Write("I want to try kickboxing.\nI like to play basketball with my son.");
+
             Console.ReadLine();
         }
     }
